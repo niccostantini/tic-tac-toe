267ba032241a23b5eb74aba5af9a69716f832fd8
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-08-13 12:57:20 +0200

    Created dialog for playerOne

--- script.js
+++ script.js
@@ -1,47 +1,85 @@
+/** SET UP */
+
 const gameboard = (() => {
     const grid = [ , , , , , , , , , ]
     return {grid}
 })()
 
+let playerOne;
+let playerTwo;
+
+const startButton = document.querySelector("#start");
+const formDialog = document.querySelector("#choosePlayers");
+const form = document.querySelector("#createPlayer");
+startButton.addEventListener("click", () => {
+    formDialog.showModal();
+})
+
+    form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    let userNick = formData.get("userNickName");
+    let userChoice = formData.get("userChoice");
+    playerOne = makePlayer(formData.get(userNick), formData.get(userChoice));
+    formDialog.close();
+
+    
+})
+
+const makeCompPlayer = (name, choice) => {
+
+}
 
 
 const makePlayer = (name, choice) => {
     const getNickName = () => name;
     const getChoice = () => choice;
+
     const makeMove = (n) => {
 
-        gameboard.grid[n] = choice;
-
         function checkWinner () {   /**CHECKS IF THE MOVE RESULTED IN A VICTORY BY THE PLAYER */
-            const winSequences = [
+            const gridTemplate = [
                 [0,1,2],
                 [3,4,5],
                 [6,7,8]
             ] 
+            var winner;
             const grid = gameboard.grid;
             /**CI VUOLE IL CICLO FOR SENNò NON VEDE TUTTE LE POSSIBILITà */
             for (let i = 0; i < 3; i++) {
-                if (grid[winSequences[i][0]] == "o"         /**CHECK HORIZONTALLY*/
-                    && grid[winSequences[i][1]] == "o"
-                    && grid[winSequences[i][2]] == "o") {
-                    console.log("WE HAVE A WINNER!")
-                } else if (grid[winSequences[0][i]] == "o"  /**CHECK VERTICALLY*/
-                    && grid[winSequences[1][i]] == "o"
-                    && grid[winSequences[2][i]] == "o") {
-                    console.log("WE HAVE A WINNER!")
-                } else if (grid[winSequences[0][0]] == "o"  /**CHECK ACROSS*/
-                    && grid[winSequences[1][1]] == "o"
-                    && grid[winSequences[2][2]] == "o") {
-                    console.log("WE HAVE A WINNER!"); break; //need to break otherwise will win 3 times (i count)
-                } else if (grid[winSequences[0][2]] == "o"  
-                    && grid[winSequences[1][1]] == "o"
-                    && grid[winSequences[2][0]] == "o") {
-                    console.log("WE HAVE A WINNER!"); break;
-                };}
+                if (grid[gridTemplate[i][0]] == choice         /**CHECK HORIZONTALLY*/
+                    && grid[gridTemplate[i][1]] == choice
+                    && grid[gridTemplate[i][2]] == choice) {
+                    console.log(`The winner is ${name}!`);
+                    winner = name;
+                } else if (grid[gridTemplate[0][i]] == choice  /**CHECK VERTICALLY*/
+                    && grid[gridTemplate[1][i]] == choice
+                    && grid[gridTemplate[2][i]] == choice) {
+                    console.log(`The winner is ${name}!`);
+                    winner = name;
+                } else if (grid[gridTemplate[0][0]] == choice  /**CHECK ACROSS*/
+                    && grid[gridTemplate[1][1]] == choice
+                    && grid[gridTemplate[2][2]] == choice) {
+                    console.log(`The winner is ${name}!`);
+                    winner = name;
+                    break; //need to break otherwise will win 3 times (i count)
+                    
+                } else if (grid[gridTemplate[0][2]] == choice  
+                    && grid[gridTemplate[1][1]] == choice
+                    && grid[gridTemplate[2][0]] == choice) {
+                    console.log(`The winner is ${name}!`);
+                    winner = name;
+                    break; //see above
 
+                };}
+        return {winner}
         };
 
-        checkWinner();
+        if (gameboard.grid[n] != undefined) {console.log(`${name}, that square is already taken, choose another!`)}
+        else if (gameboard.grid[n] == undefined) {
+            gameboard.grid[n] = choice;
+            checkWinner();}
     } 
 
     return {getNickName, getChoice, makeMove}
